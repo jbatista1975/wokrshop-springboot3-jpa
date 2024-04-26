21b19026f417dc693a264bf1974e9bd76ec32f10
joao batista <jrusob@gmail.com>
2024-04-26 20:59:54 -0300

    Project created

---
+++ src/test/java/com/batistaweb/curse/CurseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batistaweb.curse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
