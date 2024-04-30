f553c7be79b880fa6f241df034d0cfd4c5ef68d0
joao batista <jrusob@gmail.com>
2024-04-29 22:19:47 -0300

    JPA repository, dependency injection, database seeding1

--- src/main/java/com/batistaweb/curse/config/TestConfig.java
+++ src/main/java/com/batistaweb/curse/config/TestConfig.java
@@ -22,8 +22,9 @@ public class TestConfig implements CommandLineRunner {
 		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888"); 
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777"); 
+		User u3 = new User(null,"Joao Da silva" , "Jbatysta@gmail.com", "87654444");
 		
-		userRepository.saveAll(Arrays.asList(u1,u2));
+		userRepository.saveAll(Arrays.asList(u1,u2,u3));
 
 		
 	}
