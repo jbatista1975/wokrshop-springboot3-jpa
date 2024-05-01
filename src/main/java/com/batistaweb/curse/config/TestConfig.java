0a2ae32a7f003f73fb2f68e41d5cb8a507e1edff
joao batista <jrusob@gmail.com>
2024-04-30 21:46:01 -0300

    Order, Instant, ISO 8601 pat 2

--- src/main/java/com/batistaweb/curse/config/TestConfig.java
+++ src/main/java/com/batistaweb/curse/config/TestConfig.java
@@ -1,5 +1,6 @@
 package com.batistaweb.curse.config;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.batistaweb.curse.entities.Order;
 import com.batistaweb.curse.entities.User;
+import com.batistaweb.curse.repositories.OrderRepository;
 import com.batistaweb.curse.repositories.UserRepository;
 
 @Configuration
@@ -16,6 +19,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private OrderRepository orderRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -24,7 +30,15 @@ public class TestConfig implements CommandLineRunner {
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777"); 
 		User u3 = new User(null,"Joao Da silva" , "Jbatysta@gmail.com", "87654444");
 		
+		
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1); 
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2); 
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1); 
+		
+		
+		
 		userRepository.saveAll(Arrays.asList(u1,u2,u3));
+		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
 
 		
 	}
