e68991a5bfb0fee643d1842996acc57e00d39135
joao batista <jrusob@gmail.com>
2024-05-01 22:15:25 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/batistaweb/curse/entities/Product.java
+++ src/main/java/com/batistaweb/curse/entities/Product.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.hibernate.annotations.ManyToAny;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -13,6 +13,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -33,6 +34,10 @@ public class Product implements Serializable {
 	joinColumns = @JoinColumn(name = "product_id"),
 	inverseJoinColumns =   @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
+	
+	
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> Items = new HashSet<>();
 	
 	public Product() {
 	}
@@ -92,6 +97,15 @@ public class Product implements Serializable {
 
 	public Set<Category> getCategories() {
 		return categories;
+	}
+	
+	@JsonIgnore
+	public Set<Order>getOrders(){
+		Set<Order> set = new HashSet<>();
+		for(OrderItem x : Items) {
+			set.add(x.getOrder());
+		}
+		return set;
 	}
 
 	@Override
--- src/main/java/com/batistaweb/curse/repositories/OrderItemRepository.java
+++ src/main/java/com/batistaweb/curse/repositories/OrderItemRepository.java
@@ -3,7 +3,8 @@ package com.batistaweb.curse.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.batistaweb.curse.entities.OrderItem;
+import com.batistaweb.curse.entities.pk.OrderItemPK;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem,Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem,OrderItemPK> {
 
 }
